627d6cd761cf90d14f2cde9672ac8a32fbacf62c
Timw <www.tiimchernov@mail,ru>
2025-11-18 00:32:54 +0300

    task 1.1

--- tasks/chernov_t_max_matrix_columns/mpi/src/ops_mpi.cpp
+++ tasks/chernov_t_max_matrix_columns/mpi/src/ops_mpi.cpp
@@ -113,7 +113,6 @@ bool ChernovTMaxMatrixColumnsMPI::RunImpl() {
 bool ChernovTMaxMatrixColumnsMPI::PostProcessingImpl() {
   input_matrix_.clear();
   return true;
-  
 }
 
 }  // namespace chernov_t_max_matrix_columns 
